01f6be2b9416a6278105ba3553317e8d196c50fb
Aayush Bhattari <aayushbhattari30@gmail.com>
2026-01-17 01:36:31 +0530

    additional changes

--- main.py
+++ main.py
@@ -1,30 +1,73 @@
 
 
+names =[]
+phone_numbers =[]
+emails =[]
+
+
 def add():
     while True:
-        name = input('Enter Your Name: ').lower().strip()
-        if name.isdigit() or len(name)<4:
+        name = input('Enter Your Name: ').title().strip()
+        names.append(name)
+        if name.isdigit() or len(name)<3:
             print('Enter Valid Name!')
             continue
         else:
-            pass
-        while True:
+            break
+                
+    while True:
             phone_no = input("Enter your phone number: ")
             if len(phone_no) == 10 and phone_no.isdigit():
-                pass
+                phone_numbers.append(phone_no)
+                break
+                   
             else:
                 print('Enter Valid Phone Number!')
                 continue
-            while True:
-                email = input('Enter your email: ')
-                if "@" in email and email.isalnum():
-                    pass
-                else:
-                    print("Enter a valid email!")
-                    continue
+    while True:
+        email = input('Enter your email: ')
+        if "@" in email  and email.endswith('@gmail.com'):
+            emails.append(email)
+            break
+        else:
+            print("Enter a valid email!")
+            continue
 
-       
+    with open('contactbook.txt','a') as f:
+            f.write(f'Name: {name}\n')     
+            f.write(f'Phone Number: {phone_no}\n') 
+            f.write(f'Email: {email}\n') 
+            f.write('-'*40 + '\n')
 
+
+def view():
+    print('----ALL CONTACTS----')
+    with open('contactbook.txt','r') as f:   
+       data =f.read()
+       print(data)      
+  #need help here      
+def search():
+    user_search = input("Enter name or phone number to search: ").title().strip()
+    for name in names:
+        if name == user_input:
+            t = names.index(name) 
+            print('Contact Found!')
+            print(f'''Name:{user_input}
+                      Phone Number:{phone_numbers[t]}
+                      Email:{emails[t]}''')
+            
+    for phn in phone_numbers:
+        if phn == user_input:
+           t = phone_numbers.index(phn)
+        print('Contact Found!')
+        print(f'''Name:{names[t]}
+                     Phone Number:{user_input}
+                     Email:{emails[t]}''')
+        
+
+
+
+        
 
 
 
@@ -47,15 +90,16 @@ while True:
         if user_input == 1:
              add()
         elif user_input == 2:
-            pass
+             view()
         elif user_input == 3:
-            pass
+             search()
         elif user_input == 4:
             pass
         elif user_input == 5:
             pass
         elif user_input == 6:
-            pass
+            print('')
+            break
     else:
         print('Choose a Option!')
         continue
